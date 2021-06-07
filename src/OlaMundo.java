239941c04f0cdf18869938a3cccd823a9d8657e1
lordckt <henrico2002@hotmail.com>
2021-06-07 20:23:45 -0300

    Initial commit

---
+++ src/OlaMundo.java
@@ -0,0 +1,10 @@
+public class OlaMundo {
+
+    public static void main(String[] args) {
+
+        int valor1 = 22;
+        int valor2 = 23;
+
+        System.out.println(valor1 + valor2);
+    }
+}
